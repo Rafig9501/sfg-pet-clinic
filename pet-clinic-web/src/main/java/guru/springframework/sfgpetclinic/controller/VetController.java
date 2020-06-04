b66d872d1c17ee41c5048ccc5969568f715b6bb1
Rafig9501 <rafiqmammedzade@gmail.com>
2020-06-04 12:34:50 +0400

    list all Vets sent to html. closes #14

--- pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controller/VetController.java
+++ pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controller/VetController.java
@@ -1,13 +1,22 @@
 package guru.springframework.sfgpetclinic.controller;
 
+import guru.springframework.sfgpetclinic.service.VetService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class VetController {
 
+    private final VetService vetService;
+
+    public VetController(VetService vetService) {
+        this.vetService = vetService;
+    }
+
     @GetMapping({"/vet", "/vets"})
-    public String listOfVets(){
+    public String listOfVets(Model model) {
+        model.addAttribute("vets", vetService.findAll());
         return "vet/vet";
     }
 }
